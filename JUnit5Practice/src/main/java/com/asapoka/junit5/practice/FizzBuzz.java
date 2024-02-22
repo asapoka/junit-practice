b478c7035770ae8c6f2da31f07a5d9c33ec5b274
asapoka <asapoka@outlook.com>
2024-02-22 23:23:29 +0900

    add FizzBuzz

--- JUnit5Practice/src/main/java/com/asapoka/junit5/practice/FizzBuzz.java
+++ JUnit5Practice/src/main/java/com/asapoka/junit5/practice/FizzBuzz.java
@@ -7,11 +7,21 @@ package com.asapoka.junit5.practice;
 public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
-            System.out.println(fizzBuzz());
+            System.out.println(fizzBuzz(i));
         }
     }
 
-    static String fizzBuzz() {
-        return "";
+    static String fizzBuzz(int i) {
+        String out = "";
+        if (i % 3 == 0) {
+            out += "Fizz";
+        }
+        if (i % 5 == 0) {
+            out += "Buzz";
+        }
+        if (i % 3 != 0 && i % 5 != 0) {
+            out += i;
+        }
+        return out;
     }
 }
--- JUnit5Practice/src/test/java/com/asapoka/junit5/practice/FizzBuzzTest.java
+++ JUnit5Practice/src/test/java/com/asapoka/junit5/practice/FizzBuzzTest.java
@@ -1,10 +1,40 @@
 package com.asapoka.junit5.practice;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
     @Test
     void testFizzBuzz() {
+        assertEquals("FizzBuzz", FizzBuzz.fizzBuzz(15));
+        assertEquals("FizzBuzz", FizzBuzz.fizzBuzz(30));
+        assertEquals("FizzBuzz", FizzBuzz.fizzBuzz(45));
+        assertEquals("FizzBuzz", FizzBuzz.fizzBuzz(60));
+        assertEquals("FizzBuzz", FizzBuzz.fizzBuzz(75));
+        assertEquals("FizzBuzz", FizzBuzz.fizzBuzz(90));
+
+    }
+
+    @Test
+    void testFizz() {
+        assertEquals("Fizz", FizzBuzz.fizzBuzz(3));
+        assertEquals("Fizz", FizzBuzz.fizzBuzz(6));
+        assertEquals("Fizz", FizzBuzz.fizzBuzz(9));
+        assertEquals("Fizz", FizzBuzz.fizzBuzz(12));
+        assertEquals("Fizz", FizzBuzz.fizzBuzz(18));
+        assertEquals("Fizz", FizzBuzz.fizzBuzz(21));
+
+    }
+
+    @Test
+    void testBuzz() {
+        assertEquals("Buzz", FizzBuzz.fizzBuzz(5));
+        assertEquals("Buzz", FizzBuzz.fizzBuzz(10));
+        assertEquals("Buzz", FizzBuzz.fizzBuzz(20));
+        assertEquals("Buzz", FizzBuzz.fizzBuzz(25));
+        assertEquals("Buzz", FizzBuzz.fizzBuzz(35));
+        assertEquals("Buzz", FizzBuzz.fizzBuzz(40));
 
     }
 }
