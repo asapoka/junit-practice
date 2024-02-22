bfd5e1981c88fe438c2bc40ba10bc216c818c38d
asapoka <asapoka@outlook.com>
2024-02-22 23:41:32 +0900

    add comment

--- JUnit5Practice/src/main/java/com/asapoka/junit5/practice/FizzBuzz.java
+++ JUnit5Practice/src/main/java/com/asapoka/junit5/practice/FizzBuzz.java
@@ -11,6 +11,13 @@ public class FizzBuzz {
         }
     }
 
+    /**
+     * FizzBuzz
+     * 3の倍数のときはFizz,5の倍数のときはBuzz、3の倍数かつ5の倍数のときはFizzBuzz、それ以外の場合は引数の数値を返すメソッド
+     * 
+     * @param i 条件判定に利用する数値
+     * @return 判定結果から得た文字列
+     */
     static String fizzBuzz(int i) {
         String out = "";
         if (i % 3 == 0) {
